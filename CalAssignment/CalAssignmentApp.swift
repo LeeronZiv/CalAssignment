6a23602085a5faddce2ab4972a5a0402a887e7ce
leeron <40741385+LeeronZiv@users.noreply.github.com>
2024-11-19 18:42:00 +0200

    Initial Commit

---
+++ CalAssignment/CalAssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalAssignmentApp.swift
+//  CalAssignment
+//
+//  Created by Leeron Ziv on 19/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct CalAssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
